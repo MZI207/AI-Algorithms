2dced16d588929cd436e9d0fdb1b0aaf52472298
MZI207 <32904221+MZI207@users.noreply.github.com>
2020-04-08 20:56:25 -0400

    Fixed A* search and added output file

--- AISearchAlgo.py
+++ AISearchAlgo.py
@@ -1,7 +1,6 @@
 #Mohammed Iqbal
 #Mzi207
-#Create a A* Search Algorithm for the 15 puzzle problem using the Manhattan 
-#distance as a heuristic
+#Create a A* Search Algorithm for the 15 puzzle problem using the Manhattan distance as a heuristic
 import copy
 state = {} #The initial state of the 15 puzzle
 finalState = {} #goal State
@@ -55,13 +54,14 @@ def manhattanDistance(val, tempState):
 
 #Setting up the node class
 class Node:
-    def __init__(self, val, depth = 0, path = []):
+    def __init__(self, val, depth = 0, path = [], prevfx = []):
         self.val = val #The state of the node
         self.h  = 0 #The h(x) value of myself
-        for i in range(16):
+        for i in range(1,16):
             self.h += manhattanDistance(i, self.val)
-        self.depth = 0 #To get f(x) add self.h + self.depth
+        self.depth = depth #To get f(x) add self.h + self.depth
         self.path = copy.copy(path) #Path to reach me, in directions from initial state
+        self.prevfx = copy.copy(prevfx) #The previous f(x) to reach the node
 
 
 #Setting up the output file
@@ -72,15 +72,11 @@ fstream.write(originalFile)
 #The A* search algorithm
 done = True #Check to see if the state is equal to the goal state
 
-
-instructions = []
 nodes = [] 
 nodesExplored = []
 currNode = Node(state) #Node to check the nodes of the children and where to move
-for i in range(16):
-    currNode.h += manhattanDistance(i,currNode.val)
+currNode.prevfx.append(currNode.h)
 nodes.append(currNode)
-
 
 #Expands the current node and checks where to move 
 while (done):
@@ -140,57 +136,70 @@ while (done):
                 else:
                     stateRight[i] = currNode.val[i]
         #Out of the for loop
+        #Checks if the next states are valid, creating a node, once the node is created it checks if the values of the node repeats, if so it is ignored
+        #Left
         if stateLeft != {}:
-            nodeLeft = Node(stateLeft, currNode.depth + 1, currNode.path)
+            nodeLeft = Node(stateLeft, currNode.depth + 1, currNode.path, currNode.prevfx)
             nodeLeft.path.append("L")
+            nodeLeft.prevfx.append(nodeLeft.depth + nodeLeft.h)
             repeat = False
             for node in nodesExplored:
-                if node.val == nodeLeft:
+                if node.val == nodeLeft.val:
                     repeat = True
             if not repeat:
                 nodes.append(nodeLeft)
+        #Right
         if stateRight != {}:
-            nodeRight = Node(stateRight, currNode.depth + 1, currNode.path)
+            nodeRight = Node(stateRight, currNode.depth + 1, currNode.path, currNode.prevfx)
             nodeRight.path.append("R")
+            nodeRight.prevfx.append(nodeRight.depth + nodeRight.h)
             repeat = False
             for node in nodesExplored:
-                if node.val == nodeRight:
+                if node.val == nodeRight.val:
                     repeat = True
             if not repeat:
                 nodes.append(nodeRight)
+        #Up
         if stateUp != {}:
-            nodeUp = Node(stateUp, currNode.depth + 1, currNode.path)
+            nodeUp = Node(stateUp, currNode.depth + 1, currNode.path, currNode.prevfx)
             nodeUp.path.append("U")
+            nodeUp.prevfx.append(nodeUp.depth + nodeUp.h)
             repeat = False
             for node in nodesExplored:
-                if node.val == nodeUp:
+                if node.val == nodeUp.val:
                     repeat = True
             if not repeat:
                 nodes.append(nodeUp)
+        #Down
         if stateDown != {}:
-            nodeDown = Node(stateDown, currNode.depth + 1, currNode.path)
+            nodeDown = Node(stateDown, currNode.depth + 1, currNode.path,currNode.prevfx)
             nodeDown.path.append("D")
+            nodeDown.prevfx.append(nodeDown.depth + nodeDown.h)
             repeat = False
             for node in nodesExplored:
-                if node.val == nodeDown:
+                if node.val == nodeDown.val:
                     repeat = True
             if not repeat:
                 nodes.append(nodeDown)
+        #Deletes the currNode from nodes into the explored nodes since it has been searched(See A* algo for more info)
         nodesExplored.append(nodes.pop(nodes.index(currNode)))
+        
         for node in range(len(nodes)):
             if node == 0:
                 currNode = nodes[node]
             elif currNode.h + currNode.depth > nodes[node].h + nodes[node].depth:
                 currNode = nodes[node]
 
+pathStr =""
+for x in currNode.path:
+    pathStr += x + " "
 
-#Test code
-for x in range(16):
-    print(str(x) +":" + str(currNode.val[x]))
-print(currNode.path)
-"""
-for x in range(16):
-    print(str(x) +":" + str(finalState[x]))
-for x in range(16):
-    print(str(x) +":" +  str(manhattanDistance(x,currNode.val)))
-    """
+pathfxStr = ""
+for x in currNode.prevfx:
+    pathfxStr += str(x) + " "
+#Putting the result in the output file
+fstream.write(str(currNode.depth) + "\n") #The depth of the goal node
+fstream.write(str(len(nodes) + len(nodesExplored)) + "\n") #The amount of nodes generated within the tree
+fstream.write(pathStr + "\n") #The path to reach the room
+fstream.write(pathfxStr + "\n") #The f(x) of the entire path to the goal node
+
