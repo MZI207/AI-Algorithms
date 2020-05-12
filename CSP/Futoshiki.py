d5f91f751ca22f565471b6f0485ea026b130b38c
MZI207 <32904221+MZI207@users.noreply.github.com>
2020-05-12 18:14:27 -0400

    Added partial forward checking

--- CSP/Futoshiki.py
+++ CSP/Futoshiki.py
@@ -2,11 +2,10 @@
 #Mzi207
 
 initialNums = [] 
-finalNums = []
-
 hConstraints = [] # Horinzontal COnstraint
 vConstraints = [] # Vertical Constraint
 
+originalFile = ""
 while True:
     try:
         filename = input ("Filename: ")
@@ -29,6 +28,10 @@ while True:
             lines = fstream.readline()
             temp = lines.strip().split(" ")
             vConstraints.append(temp)
+        fstream.close()
+        fstream = open(filename, "r")
+        originalFile = fstream.read()
+        fstream.close()
     except NameError:
         print("Please use quotes around the file name")
         continue
@@ -38,5 +41,81 @@ while True:
     else:
         break
 
-for x in vConstraints:
-    print(x)
+
+tableofDomains = [] #Contains a list of list of list representing the world with possible domains
+#Setting up the initial domains
+
+for row in range(5):
+    tableofDomains.append([])
+    for col in range(5):
+        if initialNums[row][col] != 0:
+            tableofDomains[row].append([ initialNums[row][col] ] )
+        else:
+            tableofDomains[row].append([1,2,3,4,5])
+"""
+Could remove
+#Returns true if every element in the row is different, otherwise False
+def rowDif(row):
+    rowVals = []
+    for col in range(5):
+        if initialNums[row][col] in rowVals:
+            return False
+        rowvals.append(initialNums[row][col])
+    return True
+
+#Return true if every element in the column is different, otherwise False
+def colDif(col):
+    colVals = []
+    for row in range(5):
+        if initialNums[row][col] in colVals:
+            return False
+        colvals.append(initialNums[row][col])
+    return True
+
+"""
+#Changes the domain of the cell based on the vertical values
+def reduceVDomain(row, col):
+    if len(tableofDomains[row][col]) == 1:
+        return
+    vList = []
+    for x in range(5):
+        vList.append(initialNums[x][col])
+    for x in vList:
+        if x in tableofDomains[row][col]:
+            tableofDomains[row][col].pop(tableofDomains[row][col].index(x))
+    return
+
+#Changes the domain of the cell based on the horizontal values
+def reduceHDomain(row, col):
+    if len(tableofDomains[row][col]) == 1:
+        return
+    for x in initialNums[row]:
+        if x in tableofDomains[row][col]:
+            tableofDomains[row][col].pop(tableofDomains[row][col].index(x))
+    return 0
+
+#Forward Checking, reducing the domains for each cell using the horizontal and vertical constraints
+for row in range(5):
+    for col in range(5):
+        reduceVDomain(row,col)
+        reduceHDomain(row,col)
+        #Forward Checking with the vertical and horizontal constraints
+
+for x in range(5):
+    for y in range(5):
+        print(str(x) + "," + str(y) + ":" + str(tableofDomains[x][y]))
+
+#Back Tracking 
+#Setting up the output file
+outputFileName = "OutputOf" + filename
+fstream = open(outputFileName, "w+")
+fstream.write(originalFile)
+
+
+
+
+#---Test code---
+
+"""
+fstream.write()
+"""
